5a34fc043394528b2c371c9f217767adb8e2ed7a
Carlos Manias <carlos@maniasdiez.com>
2025-12-13 01:25:53 +0000

    Add required_providers with correct source addresses

--- terraform/modules/vps/main.tf
+++ terraform/modules/vps/main.tf
@@ -3,6 +3,17 @@
 
 terraform {
   required_version = ">= 1.0"
+
+  required_providers {
+    scaleway = {
+      source  = "scaleway/scaleway"
+      version = "~> 2.0"
+    }
+    ovh = {
+      source  = "ovh/ovh"
+      version = "~> 0.40"
+    }
+  }
 }
 
 locals {
